c8e9b4d6b1910ba8e71b684cdc6e93dd4ce5d00c
Davix019 <davidosztcs19@gmail.com>
2026-03-10 23:45:59 +0100

    Update chunk.h
    Add Chunk and TileMap base classes with memory management and raw mesh generation

--- chunk.h
+++ chunk.h
@@ -1,6 +1,7 @@
 #pragma once
 
 const int CHUNK_SIZE = 16;
+const float TILE_SIZE = 32.0f;
 
 class Chunk {
 public:
@@ -8,19 +9,28 @@ public:
     int chunkY;
     int* tiles;
 
+    float* vertices;
+    int vertexCount;
+
     Chunk(int x, int y) {
         chunkX = x;
         chunkY = y;
 
         tiles = new int[CHUNK_SIZE * CHUNK_SIZE];
 
+        vertices = new float[CHUNK_SIZE * CHUNK_SIZE * 4 * 2];
+        vertexCount = 0;
+
         for (int i = 0; i < CHUNK_SIZE * CHUNK_SIZE; ++i) {
-            tiles[i] = 0;
+            tiles[i] = 1;
         }
+
+        generateMesh();
     }
 
     ~Chunk() {
         delete[] tiles;
+        delete[] vertices;
     }
 
     int getTile(int localX, int localY) {
@@ -33,6 +43,37 @@ public:
     void setTile(int localX, int localY, int tileID) {
         if (localX >= 0 && localX < CHUNK_SIZE && localY >= 0 && localY < CHUNK_SIZE) {
             tiles[localY * CHUNK_SIZE + localX] = tileID;
+
+            generateMesh();
+        }
+    }
+
+    void generateMesh() {
+        vertexCount = 0;
+        int vertexIndex = 0;
+
+        for (int y = 0; y < CHUNK_SIZE; ++y) {
+            for (int x = 0; x < CHUNK_SIZE; ++x) {
+                int tileID = getTile(x, y);
+
+                if (tileID > 0) {
+                    float startX = (chunkX * CHUNK_SIZE + x) * TILE_SIZE;
+                    float startY = (chunkY * CHUNK_SIZE + y) * TILE_SIZE;
+
+                    vertices[vertexIndex++] = startX;
+                    vertices[vertexIndex++] = startY;
+
+                    vertices[vertexIndex++] = startX + TILE_SIZE;
+                    vertices[vertexIndex++] = startY;
+
+                    vertices[vertexIndex++] = startX + TILE_SIZE;
+                    vertices[vertexIndex++] = startY + TILE_SIZE;
+                    vertices[vertexIndex++] = startX;
+                    vertices[vertexIndex++] = startY + TILE_SIZE;
+
+                    vertexCount += 4;
+                }
+            }
         }
     }
 };
